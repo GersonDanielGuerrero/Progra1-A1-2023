eed046ff6fee2587bf5c81e8781a1beef7d0c5ba
Lesly Yuvicela Valle Guzmán <USSS036423@ugb.edu.sv>
2023-12-01 22:13:22 -0600

    Formulario de Agenda agredado

---
+++ ProyectoFinal-Progra-I/ProyectoFinal-Progra-I/Agenda.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace ProyectoFinal_Progra_I
+{
+    partial class Agenda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTituloAgenda = new System.Windows.Forms.Label();
+            this.calAgenda = new System.Windows.Forms.MonthCalendar();
+            this.btnNuevaCita = new System.Windows.Forms.Button();
+            this.btnEliminarCita = new System.Windows.Forms.Button();
+            this.btnDetallesCita = new System.Windows.Forms.Button();
+            this.grdDatosAgenda = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDatosAgenda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTituloAgenda
+            // 
+            this.lblTituloAgenda.AutoSize = true;
+            this.lblTituloAgenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloAgenda.Location = new System.Drawing.Point(82, 25);
+            this.lblTituloAgenda.Name = "lblTituloAgenda";
+            this.lblTituloAgenda.Size = new System.Drawing.Size(170, 20);
+            this.lblTituloAgenda.TabIndex = 0;
+            this.lblTituloAgenda.Text = "Agenda de la clínica";
+            // 
+            // calAgenda
+            // 
+            this.calAgenda.Location = new System.Drawing.Point(86, 73);
+            this.calAgenda.Name = "calAgenda";
+            this.calAgenda.TabIndex = 1;
+            // 
+            // btnNuevaCita
+            // 
+            this.btnNuevaCita.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevaCita.Location = new System.Drawing.Point(12, 264);
+            this.btnNuevaCita.Name = "btnNuevaCita";
+            this.btnNuevaCita.Size = new System.Drawing.Size(120, 37);
+            this.btnNuevaCita.TabIndex = 2;
+            this.btnNuevaCita.Text = "Nueva cita";
+            this.btnNuevaCita.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarCita
+            // 
+            this.btnEliminarCita.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarCita.Location = new System.Drawing.Point(157, 264);
+            this.btnEliminarCita.Name = "btnEliminarCita";
+            this.btnEliminarCita.Size = new System.Drawing.Size(120, 37);
+            this.btnEliminarCita.TabIndex = 3;
+            this.btnEliminarCita.Text = "Eliminar cita";
+            this.btnEliminarCita.UseVisualStyleBackColor = true;
+            // 
+            // btnDetallesCita
+            // 
+            this.btnDetallesCita.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetallesCita.Location = new System.Drawing.Point(304, 264);
+            this.btnDetallesCita.Name = "btnDetallesCita";
+            this.btnDetallesCita.Size = new System.Drawing.Size(120, 37);
+            this.btnDetallesCita.TabIndex = 4;
+            this.btnDetallesCita.Text = "Ver detalles";
+            this.btnDetallesCita.UseVisualStyleBackColor = true;
+            // 
+            // grdDatosAgenda
+            // 
+            this.grdDatosAgenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDatosAgenda.Location = new System.Drawing.Point(442, 64);
+            this.grdDatosAgenda.Name = "grdDatosAgenda";
+            this.grdDatosAgenda.Size = new System.Drawing.Size(442, 194);
+            this.grdDatosAgenda.TabIndex = 5;
+            // 
+            // Agenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 721);
+            this.ControlBox = false;
+            this.Controls.Add(this.grdDatosAgenda);
+            this.Controls.Add(this.btnDetallesCita);
+            this.Controls.Add(this.btnEliminarCita);
+            this.Controls.Add(this.btnNuevaCita);
+            this.Controls.Add(this.calAgenda);
+            this.Controls.Add(this.lblTituloAgenda);
+            this.Name = "Agenda";
+            this.Text = "Agenda";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.grdDatosAgenda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTituloAgenda;
+        private System.Windows.Forms.MonthCalendar calAgenda;
+        private System.Windows.Forms.Button btnNuevaCita;
+        private System.Windows.Forms.Button btnEliminarCita;
+        private System.Windows.Forms.Button btnDetallesCita;
+        private System.Windows.Forms.DataGridView grdDatosAgenda;
+    }
+}
---
+++ ProyectoFinal-Progra-I/ProyectoFinal-Progra-I/DetallesConsulta.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace ProyectoFinal_Progra_I
+{
+    partial class DetallesConsulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboPacienteDetallesConsulta = new System.Windows.Forms.ComboBox();
+            this.lblPacienteDetallesConsulta = new System.Windows.Forms.Label();
+            this.lblTituloDetallesConsulta = new System.Windows.Forms.Label();
+            this.lblVeterinarioDetallesConsulta = new System.Windows.Forms.Label();
+            this.cboVeterinarioDetallesConsulta = new System.Windows.Forms.ComboBox();
+            this.lblFechaDetallesConsulta = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblADetallesConsulta = new System.Windows.Forms.Label();
+            this.lblHoraDeDetallesConsulta = new System.Windows.Forms.Label();
+            this.lblHoraDetallesConsulta = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.lblAnamnesisDetallesConsulta = new System.Windows.Forms.Label();
+            this.txtAnamnesisDetallesConsulta = new System.Windows.Forms.TextBox();
+            this.txtDiagnosticoDetallesConsulta = new System.Windows.Forms.TextBox();
+            this.lblDiagnosticoDetallesConsulta = new System.Windows.Forms.Label();
+            this.lblExamenesDetallesConsulta = new System.Windows.Forms.Label();
+            this.lboExamenesDetallesConsulta = new System.Windows.Forms.ListBox();
+            this.lboTratamientosDetallesConsulta = new System.Windows.Forms.ListBox();
+            this.lblTratamientosDetallesConsulta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboPacienteDetallesConsulta
+            // 
+            this.cboPacienteDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboPacienteDetallesConsulta.FormattingEnabled = true;
+            this.cboPacienteDetallesConsulta.Location = new System.Drawing.Point(165, 69);
+            this.cboPacienteDetallesConsulta.Name = "cboPacienteDetallesConsulta";
+            this.cboPacienteDetallesConsulta.Size = new System.Drawing.Size(250, 28);
+            this.cboPacienteDetallesConsulta.TabIndex = 0;
+            // 
+            // lblPacienteDetallesConsulta
+            // 
+            this.lblPacienteDetallesConsulta.AutoSize = true;
+            this.lblPacienteDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPacienteDetallesConsulta.Location = new System.Drawing.Point(44, 72);
+            this.lblPacienteDetallesConsulta.Name = "lblPacienteDetallesConsulta";
+            this.lblPacienteDetallesConsulta.Size = new System.Drawing.Size(79, 20);
+            this.lblPacienteDetallesConsulta.TabIndex = 1;
+            this.lblPacienteDetallesConsulta.Text = "Paciente";
+            // 
+            // lblTituloDetallesConsulta
+            // 
+            this.lblTituloDetallesConsulta.AutoSize = true;
+            this.lblTituloDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloDetallesConsulta.Location = new System.Drawing.Point(301, 21);
+            this.lblTituloDetallesConsulta.Name = "lblTituloDetallesConsulta";
+            this.lblTituloDetallesConsulta.Size = new System.Drawing.Size(176, 20);
+            this.lblTituloDetallesConsulta.TabIndex = 2;
+            this.lblTituloDetallesConsulta.Text = "Detalles de Consulta";
+            // 
+            // lblVeterinarioDetallesConsulta
+            // 
+            this.lblVeterinarioDetallesConsulta.AutoSize = true;
+            this.lblVeterinarioDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVeterinarioDetallesConsulta.Location = new System.Drawing.Point(44, 120);
+            this.lblVeterinarioDetallesConsulta.Name = "lblVeterinarioDetallesConsulta";
+            this.lblVeterinarioDetallesConsulta.Size = new System.Drawing.Size(97, 20);
+            this.lblVeterinarioDetallesConsulta.TabIndex = 4;
+            this.lblVeterinarioDetallesConsulta.Text = "Veterinario";
+            // 
+            // cboVeterinarioDetallesConsulta
+            // 
+            this.cboVeterinarioDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboVeterinarioDetallesConsulta.FormattingEnabled = true;
+            this.cboVeterinarioDetallesConsulta.Location = new System.Drawing.Point(165, 117);
+            this.cboVeterinarioDetallesConsulta.Name = "cboVeterinarioDetallesConsulta";
+            this.cboVeterinarioDetallesConsulta.Size = new System.Drawing.Size(250, 28);
+            this.cboVeterinarioDetallesConsulta.TabIndex = 3;
+            // 
+            // lblFechaDetallesConsulta
+            // 
+            this.lblFechaDetallesConsulta.AutoSize = true;
+            this.lblFechaDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaDetallesConsulta.Location = new System.Drawing.Point(44, 164);
+            this.lblFechaDetallesConsulta.Name = "lblFechaDetallesConsulta";
+            this.lblFechaDetallesConsulta.Size = new System.Drawing.Size(59, 20);
+            this.lblFechaDetallesConsulta.TabIndex = 5;
+            this.lblFechaDetallesConsulta.Text = "Fecha";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(165, 164);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(250, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // lblADetallesConsulta
+            // 
+            this.lblADetallesConsulta.AutoSize = true;
+            this.lblADetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblADetallesConsulta.Location = new System.Drawing.Point(279, 205);
+            this.lblADetallesConsulta.Name = "lblADetallesConsulta";
+            this.lblADetallesConsulta.Size = new System.Drawing.Size(19, 20);
+            this.lblADetallesConsulta.TabIndex = 7;
+            this.lblADetallesConsulta.Text = "a";
+            // 
+            // lblHoraDeDetallesConsulta
+            // 
+            this.lblHoraDeDetallesConsulta.AutoSize = true;
+            this.lblHoraDeDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoraDeDetallesConsulta.Location = new System.Drawing.Point(161, 206);
+            this.lblHoraDeDetallesConsulta.Name = "lblHoraDeDetallesConsulta";
+            this.lblHoraDeDetallesConsulta.Size = new System.Drawing.Size(29, 20);
+            this.lblHoraDeDetallesConsulta.TabIndex = 8;
+            this.lblHoraDeDetallesConsulta.Text = "de";
+            // 
+            // lblHoraDetallesConsulta
+            // 
+            this.lblHoraDetallesConsulta.AutoSize = true;
+            this.lblHoraDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoraDetallesConsulta.Location = new System.Drawing.Point(44, 206);
+            this.lblHoraDetallesConsulta.Name = "lblHoraDetallesConsulta";
+            this.lblHoraDetallesConsulta.Size = new System.Drawing.Size(53, 20);
+            this.lblHoraDetallesConsulta.TabIndex = 9;
+            this.lblHoraDetallesConsulta.Text = "Hora:";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(196, 205);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(60, 20);
+            this.dateTimePicker2.TabIndex = 10;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(314, 206);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(60, 20);
+            this.dateTimePicker3.TabIndex = 11;
+            // 
+            // lblAnamnesisDetallesConsulta
+            // 
+            this.lblAnamnesisDetallesConsulta.AutoSize = true;
+            this.lblAnamnesisDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnamnesisDetallesConsulta.Location = new System.Drawing.Point(44, 247);
+            this.lblAnamnesisDetallesConsulta.Name = "lblAnamnesisDetallesConsulta";
+            this.lblAnamnesisDetallesConsulta.Size = new System.Drawing.Size(97, 20);
+            this.lblAnamnesisDetallesConsulta.TabIndex = 12;
+            this.lblAnamnesisDetallesConsulta.Text = "Anamnesis";
+            // 
+            // txtAnamnesisDetallesConsulta
+            // 
+            this.txtAnamnesisDetallesConsulta.Location = new System.Drawing.Point(165, 247);
+            this.txtAnamnesisDetallesConsulta.Name = "txtAnamnesisDetallesConsulta";
+            this.txtAnamnesisDetallesConsulta.Size = new System.Drawing.Size(250, 20);
+            this.txtAnamnesisDetallesConsulta.TabIndex = 13;
+            // 
+            // txtDiagnosticoDetallesConsulta
+            // 
+            this.txtDiagnosticoDetallesConsulta.Location = new System.Drawing.Point(165, 362);
+            this.txtDiagnosticoDetallesConsulta.Name = "txtDiagnosticoDetallesConsulta";
+            this.txtDiagnosticoDetallesConsulta.Size = new System.Drawing.Size(250, 20);
+            this.txtDiagnosticoDetallesConsulta.TabIndex = 15;
+            // 
+            // lblDiagnosticoDetallesConsulta
+            // 
+            this.lblDiagnosticoDetallesConsulta.AutoSize = true;
+            this.lblDiagnosticoDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiagnosticoDetallesConsulta.Location = new System.Drawing.Point(44, 362);
+            this.lblDiagnosticoDetallesConsulta.Name = "lblDiagnosticoDetallesConsulta";
+            this.lblDiagnosticoDetallesConsulta.Size = new System.Drawing.Size(104, 20);
+            this.lblDiagnosticoDetallesConsulta.TabIndex = 14;
+            this.lblDiagnosticoDetallesConsulta.Text = "Diagnóstico";
+            // 
+            // lblExamenesDetallesConsulta
+            // 
+            this.lblExamenesDetallesConsulta.AutoSize = true;
+            this.lblExamenesDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExamenesDetallesConsulta.Location = new System.Drawing.Point(44, 288);
+            this.lblExamenesDetallesConsulta.Name = "lblExamenesDetallesConsulta";
+            this.lblExamenesDetallesConsulta.Size = new System.Drawing.Size(92, 20);
+            this.lblExamenesDetallesConsulta.TabIndex = 16;
+            this.lblExamenesDetallesConsulta.Text = "Exámenes";
+            // 
+            // lboExamenesDetallesConsulta
+            // 
+            this.lboExamenesDetallesConsulta.FormattingEnabled = true;
+            this.lboExamenesDetallesConsulta.Location = new System.Drawing.Point(165, 288);
+            this.lboExamenesDetallesConsulta.Name = "lboExamenesDetallesConsulta";
+            this.lboExamenesDetallesConsulta.Size = new System.Drawing.Size(250, 56);
+            this.lboExamenesDetallesConsulta.TabIndex = 17;
+            // 
+            // lboTratamientosDetallesConsulta
+            // 
+            this.lboTratamientosDetallesConsulta.FormattingEnabled = true;
+            this.lboTratamientosDetallesConsulta.Location = new System.Drawing.Point(165, 403);
+            this.lboTratamientosDetallesConsulta.Name = "lboTratamientosDetallesConsulta";
+            this.lboTratamientosDetallesConsulta.Size = new System.Drawing.Size(250, 56);
+            this.lboTratamientosDetallesConsulta.TabIndex = 19;
+            // 
+            // lblTratamientosDetallesConsulta
+            // 
+            this.lblTratamientosDetallesConsulta.AutoSize = true;
+            this.lblTratamientosDetallesConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTratamientosDetallesConsulta.Location = new System.Drawing.Point(44, 403);
+            this.lblTratamientosDetallesConsulta.Name = "lblTratamientosDetallesConsulta";
+            this.lblTratamientosDetallesConsulta.Size = new System.Drawing.Size(114, 20);
+            this.lblTratamientosDetallesConsulta.TabIndex = 18;
+            this.lblTratamientosDetallesConsulta.Text = "Tratamientos";
+            // 
+            // DetallesConsulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 478);
+            this.Controls.Add(this.lboTratamientosDetallesConsulta);
+            this.Controls.Add(this.lblTratamientosDetallesConsulta);
+            this.Controls.Add(this.lboExamenesDetallesConsulta);
+            this.Controls.Add(this.lblExamenesDetallesConsulta);
+            this.Controls.Add(this.txtDiagnosticoDetallesConsulta);
+            this.Controls.Add(this.lblDiagnosticoDetallesConsulta);
+            this.Controls.Add(this.txtAnamnesisDetallesConsulta);
+            this.Controls.Add(this.lblAnamnesisDetallesConsulta);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.lblHoraDetallesConsulta);
+            this.Controls.Add(this.lblHoraDeDetallesConsulta);
+            this.Controls.Add(this.lblADetallesConsulta);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblFechaDetallesConsulta);
+            this.Controls.Add(this.lblVeterinarioDetallesConsulta);
+            this.Controls.Add(this.cboVeterinarioDetallesConsulta);
+            this.Controls.Add(this.lblTituloDetallesConsulta);
+            this.Controls.Add(this.lblPacienteDetallesConsulta);
+            this.Controls.Add(this.cboPacienteDetallesConsulta);
+            this.Name = "DetallesConsulta";
+            this.Text = "DetallesConsulta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboPacienteDetallesConsulta;
+        private System.Windows.Forms.Label lblPacienteDetallesConsulta;
+        private System.Windows.Forms.Label lblTituloDetallesConsulta;
+        private System.Windows.Forms.Label lblVeterinarioDetallesConsulta;
+        private System.Windows.Forms.ComboBox cboVeterinarioDetallesConsulta;
+        private System.Windows.Forms.Label lblFechaDetallesConsulta;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblADetallesConsulta;
+        private System.Windows.Forms.Label lblHoraDeDetallesConsulta;
+        private System.Windows.Forms.Label lblHoraDetallesConsulta;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.Label lblAnamnesisDetallesConsulta;
+        private System.Windows.Forms.TextBox txtAnamnesisDetallesConsulta;
+        private System.Windows.Forms.TextBox txtDiagnosticoDetallesConsulta;
+        private System.Windows.Forms.Label lblDiagnosticoDetallesConsulta;
+        private System.Windows.Forms.Label lblExamenesDetallesConsulta;
+        private System.Windows.Forms.ListBox lboExamenesDetallesConsulta;
+        private System.Windows.Forms.ListBox lboTratamientosDetallesConsulta;
+        private System.Windows.Forms.Label lblTratamientosDetallesConsulta;
+    }
+}
